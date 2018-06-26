eeb04d526e6602bbcbab071f51b0d18a19e67ca1
tfreese <commercial@freese-home.de>
2018-06-26 18:36:55 +0200

    polish

--- src/main/java/com/javaetmoi/benchmark/mapping/mapper/OrderMapper.java
+++ src/main/java/com/javaetmoi/benchmark/mapping/mapper/OrderMapper.java
@@ -3,6 +3,15 @@ package com.javaetmoi.benchmark.mapping.mapper;
 import com.javaetmoi.benchmark.mapping.model.dto.OrderDTO;
 import com.javaetmoi.benchmark.mapping.model.entity.Order;
 
-public interface OrderMapper {
-    OrderDTO map(Order source);
+/**
+ * @author Thomas Freese
+ */
+@FunctionalInterface
+public interface OrderMapper
+{
+    /**
+     * @param source {@link Order}
+     * @return {@link OrderDTO}
+     */
+    public OrderDTO map(Order source);
 }
